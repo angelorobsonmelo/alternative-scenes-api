5fc333365345fb3cc9d158c5248c1c188c70d646
angelomelo <angelorobsonmelo@gmail.com>
2019-01-31 22:46:22 -0300

    Permition all user access get event.

--- src/main/java/com/angelorobson/alternativescene/security/config/WebSecurityConfig.java
+++ src/main/java/com/angelorobson/alternativescene/security/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-				.antMatchers("/auth/**", "/v2/api-docs", "/users_app", "/states", "/cities/**", "/events/filter",
+				.antMatchers("/auth/**", "/v2/api-docs", "/users_app", "/states", "/cities/**", "/events/filter", "/events/{\\d+}",
 						"/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
 				.permitAll()
                 .anyRequest()
