593c92ed11058675a9c5f9b633fdf811f82cb53c
angelomelo <angelorobsonmelo@gmail.com>
2020-03-02 11:11:44 -0300

    change password admin to test

--- src/main/java/com/angelorobson/alternativescene/utils/PasswordUtils.java
+++ src/main/java/com/angelorobson/alternativescene/utils/PasswordUtils.java
@@ -6,25 +6,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordUtils {
 
-  private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+    private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
 
-  public PasswordUtils() {
-  }
-
-  /**
-   * Generating a hash using bcrypt.
-   *
-   * @param password
-   * @return String
-   */
-  public static String generateBCrypt(String password) {
-    if (password == null) {
-      return password;
+    public PasswordUtils() {
     }
 
-    log.info("Generate hash with BCrypt.");
-    BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
-    return bCryptEncoder.encode(password);
-  }
+    /**
+     * Generating a hash using bcrypt.
+     *
+     * @param password
+     * @return String
+     */
+    public static String generateBCrypt(String password) {
+        if (password == null) {
+            return password;
+        }
+
+        log.info("Generate hash with BCrypt.");
+        BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+        return bCryptEncoder.encode(password);
+    }
 
 }
--- src/main/resources/db/migration/postgresql/V3__insert_users.sql
+++ src/main/resources/db/migration/postgresql/V3__insert_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO user_app (google_account_id, name, email, password, profile, date_birth, image_url, phone_number, gender)
 VALUES ('google account id', 'admin da Silva', 'admin@gmail.com',
-        '$2a$06$xIvBeNRfS65L1N17I7JzgefzxEuLAL0Xk0wFAgIkoNqu9WD6rmp4m',
+        '$2a$10$JTsZa5D6fen8NiCfBH8f1uWhDcKmcRrwXPuvcLQxcZps3cAyXdOCi',
         'ROLE_ADMIN', '2001-12-19', 'https://cdn0.iconfinder.com/data/icons/PRACTIKA/256/user.png', '82 99322-9292', 'MALE');
 
 INSERT INTO user_app (google_account_id, name, email, password, profile, date_birth, image_url, phone_number, gender) VALUES
